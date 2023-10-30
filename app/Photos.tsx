87a6f085b6d4195f2c710731c00a5c6b065f45ad
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-10-30 17:12:44 +0900

    [FEAT]: Home page 퍼블리싱

---
+++ app/Photos.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+
+import photoQuery from "@/apis/query/photos.query";
+
+const PhotoItem = ({ photo }: { photo: Photo }) => {
+  return (
+    <div data-testid="photo">
+      <Image src={photo.download_url} alt={photo.url} width={300} height={300} />
+      <span>{photo.author}</span>
+    </div>
+  );
+};
+
+const Photos = () => {
+  const { data: photos } = photoQuery.getRandomPhotosQuery();
+
+  return (
+    <div className="max-w-3xl grid grid-cols-3 gap-3">
+      {photos?.map((photo) => (
+        <PhotoItem key={photo.id} photo={photo} />
+      ))}
+    </div>
+  );
+};
+
+export default Photos;
